802662f21b5252c671e3c9bd3191bb0a1325ff4d
priyed <stanleyduye@gmail.com>
2023-03-01 20:39:25 +0100

    init middleware flow

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.Use(async (context, next) => {
+    Console.WriteLine($"Logic before executing the next delegate in the Use method");
+    await next.Invoke();
+    Console.WriteLine($"Logic after executing the next delegate in the Use method");
+});
+app.Run(async context =>
+{
+    Console.WriteLine($"Writing the response to the client in the Run method");
+    await context.Response.WriteAsync("Hello from the middleware component.");
+});
+
+app.MapControllers();
+
+app.Run();
+
